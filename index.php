af4c24feb5381db0835c5b7469fc93f97f3ff7eb
Velli <emekavelli@Vellis-MacBook-Pro.local>
2022-10-18 18:04:15 +0100

    correction

--- index.php
+++ index.php
@@ -19,35 +19,29 @@
     
     <p class="payment-invoice">Invoice #4435</p>
     <h4>$25,000.00</h4>
-    <div class="form">
-      <form action="controller/form.action.php">
-        <div class="card space icon-relative">
-          <label class="label">Card holder:</label>
-          <input name="card_ho" type="text" class="input" placeholder="Steven Naismith">
-          
-        </div>
-        <div class="card space icon-relative">
-          <label class="label">Card number:</label>
-          <input name="card_no" type="text" class="input" data-mask="0000 0000 0000 0000" placeholder="1234 5678 9012 3456">
-          
-        </div>
-        <div class="card-grp space">
-          <div class="card-item icon-relative">
-            <label class="label">Expiry date:</label>
-            <input name="card_ex"type="text" name="expiry-data" class="input" data-mask="00 / 00"  placeholder="00 / 00">
-            
+    <form action="controller/form.action.php">
+      <div class="form">
+          <div class="card space icon-relative">
+            <label class="label">Card holder:</label>
+            <input name="card_ho" type="text" class="input" placeholder="Steven Naismith">
           </div>
-          <div class="card-item icon-relative">
-            <label class="label">CVC:</label>
-            <input name="cvv" type="text" class="input" data-mask="000" placeholder="000">
-            
+          <div class="card space icon-relative">
+            <label class="label">Card number:</label>
+            <input name="card_no" type="text" class="input" data-mask="0000 0000 0000 0000" placeholder="1234 5678 9012 3456">
           </div>
-        </form>
+          <div class="card-grp space">
+            <div class="card-item icon-relative">
+              <label class="label">Expiry date:</label>
+              <input name="card_ex"type="text" name="expiry-data" class="input" data-mask="00 / 00"  placeholder="00 / 00">
+            </div>
+            <div class="card-item icon-relative">
+              <label class="label">CVC:</label>
+              <input name="cvv" type="text" class="input" data-mask="000" placeholder="000">
+            </div>
+        </div>
+        <input type="submit" class="btn" value="Pay"> 
       </div>
-        
-      <input type="button" class="btn" value="Pay"> 
-      
-    </div>
+    </form>
   </div>
 </div>
 
